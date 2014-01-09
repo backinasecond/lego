2f4e98d638e4f3c6f0e09ed0b69e71c81289f68c
Dennis Gill <dennisgill.mail@gmail.com>
2014-01-09 11:07:48 +0100

    Parameter changes. Now pretty fine :)

--- src/de/gruppe2/maze/MazeSolverMain.java
+++ src/de/gruppe2/maze/MazeSolverMain.java
@@ -22,7 +22,7 @@ public class MazeSolverMain implements ButtonListener {
                 Settings.PILOT.setRotateSpeed(Settings.PILOT.getMaxRotateSpeed() / 5);
                 
                 Behavior b1 = new MazeWallHit();
-                Behavior b2 = new MazeWallFollow(7);
+                Behavior b2 = new MazeWallFollow(10);
                 
                 Behavior[] bArray = { b2, b1 };
 
